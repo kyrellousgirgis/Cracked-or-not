632a818fdd0ab9bd7f3d55f9afe0f1e720cc3657
kyrellousgirgis <37481844+kyrellousgirgis@users.noreply.github.com>
2020-07-14 08:10:48 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from numpy import expand_dims
+
+
+def load_image_pixels(filename, shape):
+	# load the image to get its shape
+	image = load_img(filename)
+	# load the image with the required size
+	image = load_img(filename, target_size=shape)
+	# convert to numpy array
+	image = img_to_array(image)
+	# scale pixel values to [0, 1]
+	image = image.astype('float32')
+	image /= 255.0
+	# add a dimension so that we have one sample
+	image = expand_dims(image, 0)
+	return image
+
+
+
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(150,150,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+batch_size = 1
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'dataset/train',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+validation_generator = test_datagen.flow_from_directory(
+        'dataset/validate',
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=119 ,
+        validation_data=validation_generator,
+        validation_steps=29 ,
+        epochs=10)
+model.save_weights('mobile_weights.h5') 
+
+#for testing
+res =  model.predict(load_image_pixels('smartphone-with-cracked-screen-vector-23551095.jpg',(150,150)))
+print('not cracked') if res[0][0]>.5  else print('cracked')
+
+
+ 
+
+
